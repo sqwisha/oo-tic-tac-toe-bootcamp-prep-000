3a5c4cd3ca090235e858c5b1787348c360bb9fe9
S H <sashanicolehart@gmail.com>
2018-09-16 00:09:28 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,7 +32,7 @@ class TicTacToe
   end 
 
   def position_taken?(index)
-     @board[index] == "X" || @board[index] == "O"
+     @board[index] == "X" || @board[index] == "O" ? true : false
   end 
 
   def valid_move?(index)
